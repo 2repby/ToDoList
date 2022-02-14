417b8167324bf8fdd16d09d7c02f0a1c719d751f
kda <2repby@gmail.com>
2022-02-14 10:03:00 +0500

    Database connect env params

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         $dotenv->load();
     }
     try {
-        $conn = new PDO("mysql:host=$_ENV[dbhost];dbname=$_ENV[dbname]", $_ENV[dbuser], $_ENV[dbpassword]);
+        $conn = new PDO("mysql:host=$_ENV[dbhost];dbname=$_ENV[dbname]", $_ENV['dbuser'], $_ENV['dbpassword']);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //        echo "Connected successfully";
