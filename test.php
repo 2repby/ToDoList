881284adffd1b2f9b5bdfb4ac7e7bdcc1ec83f72
kda <2repby@gmail.com>
2022-03-16 12:20:55 +0500

    To branch KuzinDA - conflict

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
 Проект Для групп 609-11 609-12
 <?php
     echo "Кузин Дмитрий - лидер команды";
-    echo "Dmitrii Kuzin - junior developer Здесь конфликт";
+    echo "Dmitrii Kuzin -  Здесь конфликт";
